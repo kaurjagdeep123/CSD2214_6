bc24bf7d416cb863131efe13ade8a023e01388ad
kaurjagdeep123 <46692978+kaurjagdeep123@users.noreply.github.com>
2019-01-14 17:03:47 -0500

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,15 @@
+//NAME: JAGDEEP KAUR VIRK
+//DATE: 14 JANUARY,2019
+//STUDENT: C0744298
+//COURSE:CSD 2214 SECTION 6
+
+// JavaScript source code
+alert("yeah!!!! i am a great javascript programmer!!!")
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[name="endDate"]');
+const clock = document.querySelector('clock');
+const daysSpan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');
+const mintuesSpan = clock.querySelector('.mintues');
+const secondsSpan = clock.querySelector('.seconds');
